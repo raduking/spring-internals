917788512773b498cb8f054762151d8dc3ece759
raduking <radu.lazin@boatyardx.com>
2025-04-01 15:19:39 +0300

    apiphany base updates

--- src/main/java/org/raduking/spring/internals/apitests/controller/ApiphanyController.java
+++ src/main/java/org/raduking/spring/internals/apitests/controller/ApiphanyController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping(path = { ApiClient.API })
 public class ApiphanyController {
