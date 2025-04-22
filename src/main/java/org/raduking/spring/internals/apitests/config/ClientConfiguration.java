da5a7af3167b3d8d67c7fff7ba6f624e29b08ccb
Radu Sebastian LAZIN <raduking@gmail.com>
2025-04-22 03:47:06 +0300

    added RestTemplate exchange client

--- src/main/java/org/raduking/spring/internals/apitests/config/ClientConfiguration.java
+++ src/main/java/org/raduking/spring/internals/apitests/config/ClientConfiguration.java
@@ -4,6 +4,7 @@ import org.apiphany.client.ClientProperties;
 import org.apiphany.client.ExchangeClient;
 import org.apiphany.client.http.ApacheHC5ExchangeClient;
 import org.apiphany.client.http.HttpExchangeClient;
+import org.apiphany.spring.client.RestTemplateExchangeClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -16,6 +17,8 @@ public class ClientConfiguration {
 
 	public static final String HTTP_EXCHANGE_CLIENT = "httpExchangeClient";
 	public static final String APACHE_HC5_EXCHANGE_CLIENT = "apacheHC5ExchangeClient";
+	public static final String REST_TEMPLATE_EXCHANGE_CLIENT = "restTemplateExchangeClient";
+
 	public static final String HTTP_EXCHANGE_CLIENT_PROPERTIES = "httpEchangeClientProperties";
 
 	@Bean(HTTP_EXCHANGE_CLIENT_PROPERTIES)
@@ -32,6 +35,11 @@ public class ClientConfiguration {
 	@Bean(APACHE_HC5_EXCHANGE_CLIENT)
 	ExchangeClient apacheHC5ExchangeClient(@Qualifier(HTTP_EXCHANGE_CLIENT_PROPERTIES) final ClientProperties clientProperties) {
 		return new ApacheHC5ExchangeClient(clientProperties);
+	}
+
+	@Bean(REST_TEMPLATE_EXCHANGE_CLIENT)
+	ExchangeClient restTemplateExchangeClient(@Qualifier(HTTP_EXCHANGE_CLIENT_PROPERTIES) final ClientProperties clientProperties) {
+		return new RestTemplateExchangeClient(clientProperties);
 	}
 
 }
