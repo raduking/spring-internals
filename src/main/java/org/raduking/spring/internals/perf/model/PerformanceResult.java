1d7100ea0f9ff5ad26b15fcdee1184dad7dd2b05
raduking <radu.lazin@boatyardx.com>
2025-05-05 14:57:58 +0300

    Add all settings to request

--- src/main/java/org/raduking/spring/internals/perf/model/PerformanceResult.java
+++ src/main/java/org/raduking/spring/internals/perf/model/PerformanceResult.java
@@ -8,24 +8,17 @@ public class PerformanceResult {
 	private String description;
 
 	private long count;
-	private Double average;
-	private Double p95;
-	private Double p90;
-	private Double max;
 
-	private Double totalTime;
+	private Time time;
 
 	public PerformanceResult(final String description) {
 		this.description = description;
 	}
 
-	public PerformanceResult(final String description, final DurationAccumulator accumulator) {
+	public PerformanceResult(final String description, final DurationAccumulator accumulator, final Double totalTime) {
 		this.description = description;
 		this.count = accumulator.size();
-		this.average = accumulator.average();
-		this.p90 = accumulator.percentile(95);
-		this.p95 = accumulator.percentile(90);
-		this.max = accumulator.max();
+		this.setTime(new Time(accumulator, totalTime));
 	}
 
 	@Override
@@ -49,43 +42,70 @@ public class PerformanceResult {
 		this.count = count;
 	}
 
-	public Double getAverage() {
-		return average;
+	public Time getTime() {
+		return time;
 	}
 
-	public void setAverage(final Double average) {
-		this.average = average;
+	public void setTime(Time time) {
+		this.time = time;
 	}
 
-	public Double getP95() {
-		return p95;
-	}
+	public static class Time {
 
-	public void setP95(final Double p95) {
-		this.p95 = p95;
-	}
+		private Double total;
 
-	public Double getP90() {
-		return p90;
-	}
+		private Double average;
+		private Double p95;
+		private Double p90;
+		private Double max;
 
-	public void setP90(final Double p90) {
-		this.p90 = p90;
-	}
+		public Time(DurationAccumulator accumulator, final Double total) {
+			this.total = total;
+			this.average = accumulator.average();
+			this.p90 = accumulator.percentile(95);
+			this.p95 = accumulator.percentile(90);
+			this.max = accumulator.max();
+		}
 
-	public Double getMax() {
-		return max;
-	}
+		public Double getTotal() {
+			return total;
+		}
 
-	public void setMax(final Double max) {
-		this.max = max;
-	}
+		public void setTotal(final Double totalTime) {
+			this.total = totalTime;
+		}
 
-	public Double getTotalTime() {
-		return totalTime;
-	}
+		public Double getAverage() {
+			return average;
+		}
 
-	public void setTotalTime(final Double totalTime) {
-		this.totalTime = totalTime;
+		public void setAverage(final Double average) {
+			this.average = average;
+		}
+
+		public Double getP95() {
+			return p95;
+		}
+
+		public void setP95(final Double p95) {
+			this.p95 = p95;
+		}
+
+		public Double getP90() {
+			return p90;
+		}
+
+		public void setP90(final Double p90) {
+			this.p90 = p90;
+		}
+
+		public Double getMax() {
+			return max;
+		}
+
+		public void setMax(final Double max) {
+			this.max = max;
+		}
+
 	}
 }
